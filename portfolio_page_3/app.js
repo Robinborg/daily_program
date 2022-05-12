2d7f7cc2110d3a8bca30e91dc9ff126255fe3df8
Robinborg <robin.borgatrom@gmail.com>
2022-05-12 20:59:38 +0300

    Made a third portfolio page

---
+++ portfolio_page_3/app.js
@@ -0,0 +1,26 @@
+
+
+function hideShowFirst() {
+	let x = document.getElementById("about");
+	if (x.style.display == "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "none";
+	}
+}
+function hideShowSecond() {
+	let x = document.getElementById("contact");
+	if (x.style.display == "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "none";
+	}
+}
+function hideShowThird() {
+	let x = document.getElementById("socials");
+	if (x.style.display == "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "none";
+	}
+}
