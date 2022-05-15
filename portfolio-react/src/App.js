6ff79fe3a82301fa45a1cc93372007504b53b8af
Robinborg <robin.borgatrom@gmail.com>
2022-05-15 21:19:02 +0300

    Started my React Portfolio app - setup the project structure - Made 3 components Projects, About, Navbar - Made a css file for the Projects

---
+++ portfolio-react/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Projects from './components/Projects';
+import About from './components/About';
+
+const App = () => {
+	return (
+		<>
+		<Navbar/>
+		<Projects/>
+		<About/>
+		</>
+	);
+};
+
+
+export default App;
+
---
+++ portfolio-react/src/components/Projects.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Projects.css';
+
+const Projects = () => {
+	return (
+		<div className="background-projects">
+			<div className="project-box">
+				<div className="project-item">Project</div>
+				<div className="project-item">ProjectTwo</div>
+				<div className="project-item">ProjectThree</div>
+			</div>
+		</div>
+	);
+}
+
+export default Projects;
