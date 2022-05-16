26128b38238389fcb7e9e631a54181c0804e07de
Robinborg <robin.borgatrom@gmail.com>
2022-05-16 20:48:06 +0300

    Portfolio page in React nr.2

---
+++ portfolio-react-2/src/components/About.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+	return (
+		<div className="primary-block">
+			<h1>Robin Borgstrom</h1>
+			<div className="secondary-block">
+				<p>I am a self-taught programmer.</p>
+				<p>Programming Languages: </p>
+				<ul>
+					<li>Python</li>
+					<li>Javascript</li>
+					<li>HTML</li>
+					<li>CSS</li>
+				</ul>
+				<p>I aim to understand programming on a fundamental level.</p>
+				<p>My github is updated daily</p>
+			</div>
+	  </div>
+	);
+};
+
+export default About;
