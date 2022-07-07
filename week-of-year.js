cc9e531c47936acfeb793ff90712c924474b7b07
Robinborg <robin.borgatrom@gmail.com>
2022-07-07 20:38:44 +0300

    week of year with Javascript

---
+++ week-of-year.js
@@ -0,0 +1,9 @@
+
+
+const weekYear = date => {
+	const startOfYear = new Date(date.getFullYear(), 0, 1);
+	startOfYear.setDate(startOfYear.getDate() + (startOfYear.getDay() & 7));
+	return Math.round((date - startOfYear) / (7 * 24 * 3600 * 1000));
+};
+
+console.log(weekYear(new Date("2021-07-07")));
