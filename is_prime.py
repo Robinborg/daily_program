2cd424ddb04d86bf447354f2eac2ccfd34b9b23d
Robinborg <robin.borgatrom@gmail.com>
2022-09-06 20:36:05 +0300

    is prime in python

---
+++ is_prime.py
@@ -0,0 +1,11 @@
+import math
+
+def is_prime(num):
+    for i in range(2, int(math.sqrt(num))+1):
+        if num % i == 0:
+            return False
+    return True
+
+print(is_prime(13))
+print(is_prime(14))
+print(is_prime(97))
