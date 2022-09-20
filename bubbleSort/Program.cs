5681b8e8eb4a4e7e7d21dee1d0254f5926fe24e0
Robinborg <robin.borgatrom@gmail.com>
2022-09-20 22:55:51 +0300

    started writing bubble sort and just wrote a hello in python

---
+++ bubbleSort/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class bubbleSort
+{
+		static void Main(string[] args)
+		{
+			int[] arr = {99, 92, 42, 51, 29, 11};
+			int temp = 0;
+			for (int j = 0; j < arr.Length - 2; j++)
+			{
+				for (int i = 0; i < arr.Length - 2; i++)
+				{
+					if (arr[i] > arr[i + 1])
+					{
+					  temp = arr[i + 1];
+					  arr[i + 1] = arr[i];
+					  arr[i] = temp;
+					}
+				}
+			}
+		}
+		Console.WriteLine("Sorted:");
+		foreach (int num in arr)
+		{
+			Console.WriteLine(p + " ");
+		}
+}
+
