194ebb8665ffc18f5b331df0b651dfbbd72bd231
Robinborg <robin.borgatrom@gmail.com>
2022-09-26 20:07:04 +0300

    converging function with javascript

---
+++ converge.js
@@ -0,0 +1,11 @@
+
+
+const converge = (converger, fns) => (...args) => 
+	converger(...fns.map(fn => fn.apply(null, args)));
+
+const average = converge((a, b) => a / b, [
+	arr => arr.reduce((a, v) => a + v, 0),
+	arr => arr.length
+]);
+
+console.log(average([1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9]));
