5ed3d420db1e996df3493d2e907230db10590b2b
Robinborg <robin.borgatrom@gmail.com>
2022-10-03 19:35:43 +0300

    for each with keys, values and entries

---
+++ foreach.js
@@ -0,0 +1,10 @@
+
+
+const data = [3, 4];
+Object.keys(data).forEach(k => console.log(k));
+
+Object.values(data).forEach(v => console.log(v));
+
+Object.entries(data).forEach(e => console.log(e[0], e[1]));
+
+
